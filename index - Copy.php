d26c454caeae1937ed19e560975c8eba5d72533b
trucmai2602 <hyunnguyen2503@gmail.com>
2023-04-22 21:00:07 +0700

    first commit

---
+++ index - Copy.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<h1> Kiểm tra pass , ngày 10/4/2023  </h1>
+<h2>nguyen trung thong b2013505 </h2>
+	<ul>
+		<li>
+			<a href="createtable.php">Create Table Student</a>
+		</li>
+		<li><a href="addsv.php">Add Student</a></li>
+		<li>
+			<a href="list.php">List Student</a>
+		</li>
+		
+	</ul>
+</body>
+</html>
+
